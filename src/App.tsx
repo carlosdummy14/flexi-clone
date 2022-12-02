93d6c38972cb894479c4a1d00156d7a166d41a53
Carlos <carlosdummy14@gmail.com>
2022-12-02 10:30:50 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='w-100 flex justify-center bg-red-300'>
+      <h3 className='text-3xl font-bold underline'>Flexi clone</h3>
+    </div>
+  )
+}
+
+export default App
